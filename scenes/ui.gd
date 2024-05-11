8d6e2265e4a8b2bfd52c3acc95eeaeb60ffbd412
Nicolas Kotolenko <nicolas.kotolenko@etermax.com>
2024-05-11 13:36:43 -0300

    added pause menu and return to main menu

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 var is_paused: bool = false
+@onready var pause_menu = $pause_menu
 
 func _process(_delta):
 	check_if_pause()
@@ -9,8 +10,13 @@ func check_if_pause():
 	if Input.is_action_just_pressed("pause"):
 		if get_tree().paused:
 			get_tree().paused = false
-			print("unpaused")
+			deactivate_pause_ui()
 		else:
 			get_tree().paused = true
-			print("paused")
-	
+			activate_pause_ui()
+
+func activate_pause_ui():
+	pause_menu.visible = true
+
+func deactivate_pause_ui():
+	pause_menu.visible = false
