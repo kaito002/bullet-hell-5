15ab7cca24ea8d31822357d429be028c94f07656
Nicolas Kotolenko <nicolas.kotolenko@etermax.com>
2024-05-11 10:19:48 -0300

    REF small change in weapon script

--- scenes/weapons/weapon.gd
+++ scenes/weapons/weapon.gd
@@ -5,9 +5,12 @@ extends Node2D
 @onready var debounce: Timer = $debounce
 
 func fire():
-	if !debounce.is_stopped(): return
-	
+	if is_on_cooldown(): return
 	debounce.start()
+	
 	var bullet: Bullet = bullet_scene.instantiate()
 	bullet.global_position = global_position
 	get_tree().root.add_child(bullet, true)
+
+func is_on_cooldown():
+	return !debounce.is_stopped()
