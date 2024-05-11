c0b2a3fd9ea5403ff879a45f2dd49652d6b3e4b2
Nicolas Kotolenko <nicolas.kotolenko@etermax.com>
2024-05-11 16:28:49 -0300

    bullets are now child to weapons

--- scenes/weapons/weapon.gd
+++ scenes/weapons/weapon.gd
@@ -16,11 +16,10 @@ func fire(target_direction: Vector2 = direction):
 	debounce.start()
 	
 	var bullet: Bullet = bullet_scene.instantiate()
-	bullet.global_position = global_position
 	bullet.direction = target_direction
 	bullet.rotation = target_direction.angle()
 	bullet.rotation_degrees += 90
-	get_tree().root.add_child.call_deferred(bullet, true)
+	add_child.call_deferred(bullet, true)
 
 func is_on_cooldown():
 	return !debounce.is_stopped()
