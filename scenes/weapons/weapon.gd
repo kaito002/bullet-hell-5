b00fde0c2163aae63f43d1156ce4b1c30bdbf8a5
Nicolas Kotolenko <nicolas.kotolenko@etermax.com>
2024-05-11 14:05:17 -0300

    added fire rate to weapons

--- scenes/weapons/weapon.gd
+++ scenes/weapons/weapon.gd
@@ -3,10 +3,12 @@ extends Node2D
 
 @export var bullet_scene: PackedScene
 @export var direction: Vector2 = Vector2.UP
+@export var fire_rate: float = 3.0
 @onready var debounce: Timer = $debounce
 
 func _ready():
 	direction = direction.normalized()
+	debounce.wait_time = 1/fire_rate
 
 
 func fire(target_direction: Vector2 = direction):
