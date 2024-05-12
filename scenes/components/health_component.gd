d1b73fbdf9324542dd4df10742654c9b14f742e4
Carlos Montes <carlosmontes002@gmail.com>
2024-05-12 12:14:17 -0300

    add tilt animation when enemy was hitted

--- scenes/components/health_component.gd
+++ scenes/components/health_component.gd
@@ -1,18 +1,31 @@
 class_name HealthComponent
 extends Node
 
+@onready var tilt_timer: Timer = $tilt_timer
+
 @export var parent: Node
 @export var max_lives: int = 50
 var lives: int
+var body: Node2D 
 
 func _ready():
 	parent = get_parent()
+	body = parent.get_node("Art")
 	lives = max_lives
 
 func hit():
 	lives  = max(0, lives - 1)
+	_on_hit()
 	if lives == 0:
 		death()
 
 func death():
 	parent.call_deferred("queue_free")
+	
+func _on_hit():
+	body.modulate = Color("b90a00")
+	tilt_timer.start()
+
+
+func _on_tilt_timer_timeout() -> void:
+	body.modulate = Color("ffffff")
