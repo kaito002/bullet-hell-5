4f436b2ba06cdc21a1bd6874b147587f245976a6
Nicolas Kotolenko <nicolas.kotolenko@etermax.com>
2024-05-12 11:29:01 -0300

    throw error when enemy is missing health component

--- scenes/components/health_component.gd
+++ scenes/components/health_component.gd
@@ -1,3 +1,4 @@
+class_name HealthComponent
 extends Node
 
 @export var parent: Node
--- scenes/weapons/bullet.gd
+++ scenes/weapons/bullet.gd
@@ -10,5 +10,12 @@ func _physics_process(delta):
 
 func _on_area_entered(area: Node):
 	if area.is_in_group("enemies"):
-		area.get_node("./health_component").hit()
+		var health_component: HealthComponent = area.get_node("./health_component")
+		if health_component != null:
+			deal_damage(health_component)
+		else:
+			push_error(area.name, " does not have a health_component!")
 		call_deferred("queue_free")
+
+func deal_damage(health_component: HealthComponent):
+	health_component.hit()
