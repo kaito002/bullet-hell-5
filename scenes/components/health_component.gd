4486364711f80dd9eb7b1b4df3e94d15ac758bb7
Nicolas Kotolenko <nicolas.kotolenko@etermax.com>
2024-05-12 13:49:58 -0300

    health component now has different colors for health percentage

--- scenes/components/health_component.gd
+++ scenes/components/health_component.gd
@@ -23,9 +23,19 @@ func death():
 	parent.call_deferred("queue_free")
 	
 func _on_hit():
-	body.modulate = Color("b90a00")
+	body.modulate = get_color()
 	tilt_timer.start()
-
 
 func _on_tilt_timer_timeout() -> void:
 	body.modulate = Color("ffffff")
+	
+func get_color() -> Color:
+	var health_percentage: float = (float(lives)/max_lives) * 100
+	#print("max lives: ", max_lives, " | lives: ", lives, " | percentage: ", health_percentage)
+	
+	if health_percentage > 70:
+		return Color.GREEN_YELLOW
+	elif health_percentage > 30:
+		return Color.YELLOW
+	else:
+		return Color.RED
