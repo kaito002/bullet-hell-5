afe257bc20dd7ba93677969d03be5534e637e86f
Nicolas Kotolenko <nicolas.kotolenko@etermax.com>
2024-05-11 21:46:32 -0300

    taurus bullet extends bullet

--- scenes/enemies/taurus/taurus_bullet.gd
+++ scenes/enemies/taurus/taurus_bullet.gd
@@ -1,11 +1,13 @@
 class_name TaurusBullet
-extends Area2D
+extends Bullet
 
 @onready var sprite: Sprite2D = $NoseRing
-
-var direction: Vector2 = Vector2.ZERO
-var speed: float = 0
 
 func _process(delta: float) -> void:
 	sprite.rotate(2*PI * delta)
 	position += speed * direction * delta
+
+
+func _on_body_entered(body):
+	if body.is_in_group('player'):
+		body.hit()
