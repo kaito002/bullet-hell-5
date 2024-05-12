21aa96123777cfa0e7ed781b96cbfb8d4edf0e23
Nicolas Kotolenko <nicolas.kotolenko@etermax.com>
2024-05-11 22:23:28 -0300

    changed player script name

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,4 +1,4 @@
-class_name PlayerMovement
+class_name Player
 extends CharacterBody2D
 
 @onready var weapon: Weapon = $weapon
@@ -7,7 +7,6 @@ extends CharacterBody2D
 var direction: Vector2 = Vector2.ZERO
 
 func _physics_process(_delta):
-	
 	velocity = direction * speed
 	move_and_slide()
 	
